3e9aaf5106a39d76fda0ac33505e3746681d2908
Idooru <shere1765@gmail.com>
2022-08-28 19:56:20 +0900

    Add some example

--- src/main/java/lambda/B005.java
+++ src/main/java/lambda/B005.java
@@ -8,6 +8,12 @@ public class B005 {
 
         int result = mfi.runSomething(3);
         System.out.println(result);
+
+        System.out.println(mfi2.runSomething(4));
     }
+
+    static MyFunctionalInterface mfi2 = (int a) -> {
+        return a * a;
+    };
 }
 
